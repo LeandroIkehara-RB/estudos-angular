32433995e4a979e74f50b84510d8ce5ac617d8ee
LeandroIkehara-RB <likehara@rastreabilidadebrasil.com.br>
2022-03-18 10:52:42 -0300

    SwitchMap e Merge

--- bytebank-rxjs/byte-bank-broker/src/app/acoes/acoes.component.ts
+++ bytebank-rxjs/byte-bank-broker/src/app/acoes/acoes.component.ts
@@ -1,8 +1,8 @@
-import { Subscription } from 'rxjs';
+import { switchMap, tap } from 'rxjs/operators';
 import { AcoesService } from './acoes.service';
-import { Acoes } from './modelo/acoes';
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { merge } from 'rxjs';
 
 @Component({
   selector: 'app-acoes',
@@ -11,19 +11,25 @@ import { FormControl } from '@angular/forms';
 })
 export class AcoesComponent {
   acoesInput = new FormControl();
-  //acoes: Acoes;
-  //private subscription: Subscription;
-  acoes$ = this.acoesService.getAcoes();
+  //acoes$ = this.acoesService.getAcoes();
+  todasAcoes$ = this.acoesService.getAcoes().pipe(
+    tap(() => {
+      console.log('Fluxo Inicial');
+    })
+  );
+  filtroPorInput$ = this.acoesInput.valueChanges.pipe(
+    tap(() => {
+      console.log('Fluxo do Filtro');
+    }),
+    switchMap((valorDigitado) => this.acoesService.getAcoes(valorDigitado))
+  );
+  // acoes$ = this.acoesInput.valueChanges.pipe(
+  //   tap(console.log),
+  //   switchMap((valorDigitado) => this.acoesService.getAcoes(valorDigitado)),
+  //   tap(console.log)
+  // );
+
+  acoes$ = merge(this.todasAcoes$, this.filtroPorInput$);
 
   constructor(private acoesService: AcoesService) {}
-
-  // ngOnInit() {
-  //   this.subscription = this.acoesService.getAcoes().subscribe((acoes) => {
-  //     this.acoes = acoes;
-  //   });
-  // }
-
-  // ngOnDestroy() {
-  //   this.subscription.unsubscribe();
-  // }
 }
